3d45c0c0117c4eaf8b24015b86abf62835e8d82f
Tuan Le <leanhtuan030523@gmail.com>
2024-10-07 18:55:14 +0700

    Add is_id_exists function

--- task.py
+++ task.py
@@ -19,3 +19,14 @@ def create_new_task(description: str):
 def update_task_file(new_file_data):
     with open(TASKS_FILE, "w") as f:
         json.dump(new_file_data, f, indent=4)
+        
+def is_id_exists(task_id: int) -> bool:
+    with open(TASKS_FILE, "r") as f:
+        file_data = json.load(f)
+        tasks = file_data["task_list"]
+        
+        for task in tasks:
+            if task["id"] == task_id:
+                return True
+        
+        return False
