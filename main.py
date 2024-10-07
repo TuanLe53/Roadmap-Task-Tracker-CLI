156aaebec93d8a9e27b3c651d7af827abad5c0bd
Tuan Le <leanhtuan030523@gmail.com>
2024-10-07 08:38:05 +0700

    Add delete_task command

--- main.py
+++ main.py
@@ -15,6 +15,17 @@ def add(description: str):
         file_data["task_list"].append(new_task)
         file.seek(0)
         json.dump(file_data, file, indent=4)
+        
+@app.command()
+def delete(task_id: int):
+    
+    with open(TASKS_FILE, "r") as file:
+        file_data = json.load(file)
+        tasks = file_data["task_list"]
+        
+        file_data["task_list"] = [task for task in tasks if task["id"] != task_id]
 
+    update_task_file(file_data)
+        
 if __name__ == "__main__":
     app() 
